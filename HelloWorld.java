6182daf32fbab91fb5684c6486caf9b9da926329
manasa84 <kmanasareddy@gmail.com>
2017-06-30 14:40:43 -0400

    Update HelloWorld.java
    test build

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,11 @@
 package com.fannie;
-
+//test
 public class HelloWorld {
 	
 public static void main(String [] args) {
 	System.out.println("welcome to Java from TIBCO");
 for (int i=0; i>10;i++){
-     System.out.println("welcome to Java from TIBCO");
+     System.out.println("welcome to Java from GIT ");
 }
-}	
+}	 
 }
